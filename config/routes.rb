add5269aca95eec1bacfd455e11c6dbb696b4aa4
Sherief Wissa <sheriefwissa@BISHOP.local>
2018-05-08 17:05:19 +1000

    1. Added addition gem Faker & CSV) 2. Changed seed file. 3. Created Page controller 4. Added new route to point to Home page 5. Created pages/index.html.erb (which is the home page)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :mybooks
   resources :books
   resources :users
+  resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'pages#index'
 end
