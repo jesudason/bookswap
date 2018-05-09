0a25dc7dc068436c866651f3aa9a4b1a15c9a5e5
Nicole Jesudason <nicole.jesudason@gmail.com>
2018-05-09 11:15:47 +1000

    login form update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,25 @@
 class UsersController < ApplicationController
 
     def new
-    
+        @user = User.new
     end 
 
     def create
-        user = User.new
-        user.first_name = params[:first_name]
-        user.last_name = params[:last_name]
-        user.phone = params[:phone]
-        user.email = params[:email]
-        user.address = params[:address]
-        user.country = params[:country]
-        user.suburb = params[:suburb]
-        user.username = params[:username]
-        user.password = params[:password]
-        user.profile_url = params[:profile_pic]
-        user.save
-    
+        @user = User.new(create_params)
+        if @user.save
+            redirect_to('/')
+        else
+            @errors = @user.errors.full_messages
+            render :new
+        end
     end 
 
     def show
 
+    end
+
+    def create_params
+        params.require(:user).permit(:first_name, :last_name, :phone, :email, :address, :country, :suburb, :username, :password, :profile_pic)
     end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password	
-    validates_presence_of :first_name, :last_name, :email, :address, :country, :suburb, :username, :password, :profile_url
+    validates_presence_of :first_name, :last_name, :email, :address, :country, :suburb, :username, :profile_url
     validates_uniqueness_of :email, :case_sensitive => false
     validates :password, confirmation: { case_sensitive: true }
 
